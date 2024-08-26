5048d091585aaaba69d19d778b843726af11dece
Alan Padilla Chua <alanpadillachua@gmail.com>
2024-08-26 19:23:04 -0400

    subscribers model created

---
+++ app/models/subscribers.rb
@@ -0,0 +1,18 @@
+class Subscribers < ApplicationRecord
+
+  before_validation :formatEmail 
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, email:true, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :status, presence: true, inclusion: ['active', 'inactive']
+
+
+  private
+  
+  def formatEmail
+    self.email = email.downcase.strip if email.prensent?
+  end
+  
+
+end
